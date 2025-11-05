c71ae70f67f07e3a8e14cb5fa3464c7865656441
prabhakar1623 <prabhakaran.r@pyramidions.in>
2025-11-05 12:28:37 +0530

    Fix lint and test issues

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
+
 def add(a, b):
     return a + b
 
 def subtract(a, b):
     return a - b
+
 print("prabhu test")
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,9 +1,14 @@
-import sys, os
+import sys
+import os
+
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 from app import add, subtract
+
 
 def test_add():
     assert add(2, 3) == 5
+
 
 def test_subtract():
     assert subtract(6, 4) == 2
