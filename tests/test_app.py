60b72b2c74fd1b611fc872f7b2daad2542b9d1e3
prabhakar1623 <prabhakaran.r@pyramidions.in>
2025-11-10 15:18:20 +0530

    checking pull

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,13 +1,68 @@
-import sys
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException, TimeoutException
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select
+import time
 import os
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-from app import add, subtract
 
+service = Service(r"C:\chromedriver-win64\chromedriver.exe")  
+driver = webdriver.Chrome(service=service)
 
-def test_add():
-    assert add(2, 3) == 5
+driver.maximize_window()
+driver.get("https://app.meta-path.ai/cdHome/login")
+gmail=driver.find_element(By.ID,"username")
+gmail.send_keys("prabhakar_QACND924150")
+password=driver.find_element(By.ID,"password")
+password.send_keys("fKdW1gep")
+next=driver.find_element(By.XPATH,"//input[@value='Continue']")
+next.click()
+time.sleep(10)
+#checking for accept agreement
+try:
+    check=driver.find_element(By.XPATH,"//span[@class='checkmark']")
+    check.click()
+    time.sleep(10)
+    element = driver.find_element(By.XPATH,"//button[normalize-space()='continue']")
+    element.click()
+except NoSuchElementException:
+# Element not found — skip to next step
+    print("'Continue' button not found, skipping this step...")
 
+ #pan document
+pan=driver.find_element(By.XPATH,"//div[7]//div[1]//div[1]//div[2]")
+pan.click()
+time.sleep(3)
+#pan uploaded path
+folder_path = r"C:\\Users\\prabhakaran\\Downloads\\New folder"
+file_name= "pan card.jpeg"
+file_path = os.path.join(folder_path, file_name)
+upload_input = driver.find_element(By.XPATH, "//input[@type='file']")
+upload_input.send_keys(file_path)
+driver.find_element(By.XPATH,"//input[@id='fathername']").send_keys("Ramamoorthy")
+wait = WebDriverWait(driver, 3)
+for _ in range(3):  # retry up to 3 times if stale
+        try:
+            wait.until(EC.presence_of_element_located((By.XPATH, "//select[@id='gender']")))
+            dropdown = driver.find_element(By.XPATH, "//select[@id='gender']")
+            Select(dropdown).select_by_visible_text("Male")
+            print(" Gender selected as 'Male'")
+            break
+        except StaleElementReferenceException:
+            print(" Dropdown became stale retrying...")
+driver.find_element(By.XPATH,"//input[@id='bloodgroup']").send_keys("B+")
+driver.find_element(By.XPATH,"//div[@class='logo_upload_wrapper']").click()
+time.sleep(1)
+driver.find_element(By.Xpath,"//div[@class='col-12 col-sm-12']//div[1]//div[1]").click()
+folder_path_my= r"C:\\Users\\prabhakaran\\Downloads\\New folder"
+file_name_my= "my pic.jpeg"
+file_path_my = os.path.join(folder_path_my, file_name_my)
+upload_input = driver.find_element(By.XPATH, "//input[@type='file']")
+upload_input.send_keys(file_path_my)
 
-def test_subtract():
-    assert subtract(6, 4) == 2
+time.sleep(30)
